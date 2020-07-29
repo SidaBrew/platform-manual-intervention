2d9a399207670d04ccbbc123da9612a5edaf12c8
luolei <luolei@shangzhao360.com>
2020-07-29 11:31:41 +0800

    测试获取配置文件中的数组

--- src/main/java/com/zenki/dataplus/pmi/controller/TestController.java
+++ src/main/java/com/zenki/dataplus/pmi/controller/TestController.java
@@ -5,9 +5,13 @@ import com.zenki.dataplus.pmi.service.impl.LoginServiceImpl;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /***
  *  Created by shao.guangze on 2020/5/31
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Api(value = "Demo2-API", description = "测试Demo2")
 @RestController
+@RequestMapping("testWhiteList")
 public class TestController {
 
     @Autowired
@@ -23,11 +28,20 @@ public class TestController {
     @Autowired
     private LoginServiceImpl loginService;
 
+    @Value("#{'${com.ruoyi.project.whiteList}'.split(',')}")
+    private List<String> whiteList;
+
     @ApiOperation(value = "创建对象", notes = "根据User对象创建用户")
     @RequestMapping(value = "/hello", method = RequestMethod.POST)
-    public String getHello(){
+    public String getHello(HttpServletRequest request){
 
+        System.out.println(request.getRequestURL());
+        System.out.println(request.getRequestURI());
 
+        if (whiteList.contains(request.getRequestURI())) {
+            System.out.println("可以进行接口请求");
+        }
         return  loginService.login();
     }
+
 }
--- src/main/java/com/zenki/dataplus/pmi/service/impl/TestServiceImpl.java
+++ src/main/java/com/zenki/dataplus/pmi/service/impl/TestServiceImpl.java
@@ -1,7 +1,5 @@
 package com.zenki.dataplus.pmi.service.impl;
 
-import com.zenki.dataplus.pmi.dao.generated.entity.BlobTest;
-import com.zenki.dataplus.pmi.dao.generated.mappper.BlobTestMapper;
 import com.zenki.dataplus.pmi.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,19 +9,23 @@ import org.springframework.stereotype.Service;
  */
 @Service("TestServiceImpl")
 public class TestServiceImpl implements TestService {
-
-
-    @Autowired
-    private BlobTestMapper blobTestMapper;
-
-    int insert = 1;
     @Override
     public String method() {
-        BlobTest blobTest = new BlobTest();
-        blobTest.setId(111267);
-        byte[] aByte = {10};
-        blobTest.setFileContent(aByte);
-        int insert = blobTestMapper.insert(blobTest);
-        return insert+"";
+        return null;
     }
+
+
+//    @Autowired
+//    private BlobTestMapper blobTestMapper;
+//
+//    int insert = 1;
+//    @Override
+//    public String method() {
+//        BlobTest blobTest = new BlobTest();
+//        blobTest.setId(111267);
+//        byte[] aByte = {10};
+//        blobTest.setFileContent(aByte);
+//        int insert = blobTestMapper.insert(blobTest);
+//        return insert+"";
+//    }
 }
